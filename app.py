4d3899a440ee9720ec6d1477876adbf6256e314b
Bajaj <rajesh.bajaj@accenture.com>
2024-05-10 16:15:09 +0530

    added Multiplication to app file

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from cal_func import do_addition, do_subtraction
+from multiply import do_multiplication
 def main():
     print("welcome to calculator app")
     print("""
           \nSelect the funciton from the given options
           1. Add
           2. Subtract
+          3. Multiplication
           """)
     user_input = input("select the function")
 
@@ -15,6 +17,8 @@ def main():
         result = do_addition(a,b)
     elif user_input == "2":
         result = do_subtraction(a,b)
+    elif user_input =="3":
+        result = do_multiplication(a,b)
     
     print("Result : ", result)
 
